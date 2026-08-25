1637c6bf7ab31f76308a7a89057a0f6f2233cff2
Elias <eliasraimundo2005@gmail.com>
2026-08-25 20:25:46 -0300

    GET Y SET

--- src/main/java/bodega_system/dto/DashboardStats.java
+++ src/main/java/bodega_system/dto/DashboardStats.java
@@ -6,4 +6,10 @@ public class DashboardStats {
     public long lowStock;
     public double inventoryValue;
     public double costValue;
+
+    public long getTotalProducts() { return totalProducts; }
+    public Double getTotalStock() { return totalStock; }
+    public long getLowStock() { return lowStock; }
+    public double getInventoryValue() { return inventoryValue; }
+    public double getCostValue() { return costValue; }
 }
